43f6ab170784edce49ee6f3cce6ef4feb70dc68b
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-02-27 14:33:04 +0100

    installerar databas

---
+++ app.js
@@ -0,0 +1,12 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyCdAnlmX_mxn81UBIBOdlpZdiSmAjzmH10',
+    authDomain: 'fe21-js2-801d6.firebaseapp.com',
+    projectId: 'fe21-js2-801d6'
+  });
+  
+  var db = firebase.firestore();
+
+
+
+  
