4d935773cfac310ef7b19ac8d6c0804c56ec4fc9
Ayub Ali Bare <Ayublul@gmail.com>
2022-02-27 23:34:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ ShoppingCart.prototype.addItem = function(itemName, quantity , price){
 
 //remove item method
 
+ShoppingCart.prototype.removeItem = function(itemName, quantity, pric){
+    //deduct the cost of the items that have been removed from the shopping cart
+    this.total = price*quantity;
+
+    //remove items from the items dict
+    this.items[itemName]= this.items[itemName] - quantity ;
+
+};
+
+// checkout method
+
+ShoppingCart.prototype.checkout = function(cashPaid){
+    if(cashPaid < this.total){
+        return "Cash paid not enough";
+    }
+    return cashPaid-this.total;
+};
+
+
+//shop, sub class of ShoppinCart
+function Shop() {
+    this.quantity =100;
+}
+Shop.prototype = Object.create(ShoppingCart.prototype);
+//remove item method
+Shop.prototype.removeItem = function(){
+    //remove items from the items dict
+    this.quantity = this.quantity-1;
+};
+
