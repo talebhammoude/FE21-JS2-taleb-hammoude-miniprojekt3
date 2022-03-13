1c31b4e735c6baedc3f0fa06d2b88b4ed017e96b
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-03-13 23:19:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: 'AIzaSyCdAnlmX_mxn81UBIBOdlpZdiSmAjzmH10',
-    authDomain: 'fe21-js2-801d6.firebaseapp.com',
-    projectId: 'fe21-js2-801d6'
-  });
-  
-  var db = firebase.firestore();
-
-
-
-
-
-
-  class Product {
-    constructor(namn, pris, lagersaldo) {
-
-        this.namn=namn;
-        this.pris=pris;
-        this.pris=lagersaldo;
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log("draw");
     }
-  }
+}
 
 
+const another = new Circle(1);
 
-  function readFromDb() {
 
-    db.collection("products").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(doc.data());
-            var addProduct = new Product(doc.data().namn, doc.data().pris, doc.data().lagersaldo);
-
-            
-        });
-    });
-
-  }
-
+another.draw();
